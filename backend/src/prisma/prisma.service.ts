0457143dd059cc331a4b12a2104322de44564017
Mr. AKS <aksnetlink@gmail.com>
2026-02-02 13:50:13 +0530

    fix: explicit Prisma db connection config

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,6 +3,17 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        super({
+            // @ts-ignore
+            datasources: {
+                db: {
+                    url: process.env.DATABASE_URL,
+                },
+            },
+        });
+    }
+
     async onModuleInit() {
         try {
             await this.$connect();
